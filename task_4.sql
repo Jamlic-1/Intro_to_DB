b2a8dc7007f89986181216116651b77712cfe21d
Jamlic <150439323+Jamlic-1@users.noreply.github.com>
2025-10-20 00:03:49 +0300

    Replace column details query with table creation query

--- task_4.sql
+++ task_4.sql
@@ -1,8 +1,2 @@
--- Use the correct database
 USE alx_book_store;
-
--- Show the current database in use
-SELECT DATABASE();
-
--- Show full column details of the table 'books'
-SHOW COLUMNS FROM Books;
+SHOW CREATE TABLE books;
