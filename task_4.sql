8e46aa8260256b5c8a6d5a95e846d08015a9ed29
Jamlic <150439323+Jamlic-1@users.noreply.github.com>
2025-10-19 23:52:46 +0300

    Add commands to show current database and table structure

--- task_4.sql
+++ task_4.sql
@@ -1,3 +1,5 @@
-SELECT COLUMN_NAME, COLUMN_TYPE, IS_NULLABLE, COLUMN_KEY, COLUMN_DEFAULT, EXTRA
-FROM INFORMATION_SCHEMA.COLUMNS
-WHERE TABLE_SCHEMA = 'alx_book_store' AND TABLE_NAME = 'books';
+-- Show the current database
+SELECT DATABASE();
+
+-- Display the full description of the 'books' table
+SHOW CREATE TABLE Books;
